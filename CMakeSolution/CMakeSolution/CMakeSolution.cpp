769ef562498482c3553a17ac8da811076e33697b
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2021-02-09 21:11:01 +0100

    Add VS CMakeSolution with 'Hello World Code' and proper gitignore

---
+++ CMakeSolution/CMakeSolution/CMakeSolution.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeSolution.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeSolution.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World by CMake." << endl;
+	string s;
+	cin >> s;
+	return 0;
+}
